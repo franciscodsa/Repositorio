a14512dffe58308d717f04a804e50780c0eb9e24
francisco <franciscojesusdasilvaarana@gmail.com>
2022-09-27 19:53:29 +0200

    Repositorio

---
+++ src/VariablesYOperadores/Ejercicio12.java
@@ -0,0 +1,19 @@
+package VariablesYOperadores;
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nume = sc.nextInt();
+
+        int num1= nume/100;
+        int num2= (nume%100)/10;
+        int num3= (nume%100)%10;
+
+        System.out.print(num3);
+        System.out.print(num2);
+        System.out.print(num1);
+    }
+
+}
